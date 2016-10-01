5339ce9f77fa9368a7a4c1837d4034480145f65a
Kathleen <github email address>
2016-10-01 03:02:36 -0400

    create restaurant model

---
+++ db/migrate/20161001061424_create_restaurant.rb
@@ -0,0 +1,11 @@
+class CreateRestaurant < ActiveRecord::Migration[5.0]
+  def change
+    create_table :restaurants do |t|
+    	t.string :name
+    	t.string :address
+    	t.string :status, default: nil
+    	t.integer :license
+    end
+  end
+end
+
