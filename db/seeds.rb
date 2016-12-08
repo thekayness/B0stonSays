0eb1c9dc19e3eca9f450077b81e321f17d4b6f87
Kathleen <kayjsmyth@gmail.com>
2016-12-07 20:41:20 -0500

    commented out seeds populating db elsewhere

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-client = SODA::Client.new({:domain => "data.cityofboston.gov", :app_token => "9qklEHKAEThh6thj4O7n4YBQI"})
+# client = SODA::Client.new({:domain => "data.cityofboston.gov", :app_token => "9qklEHKAEThh6thj4O7n4YBQI"})
 
 
-results = client.get("427a-3cn5", :$limit => 500)
-results.each do |result|
+# results = client.get("427a-3cn5", :$limit => 500)
+# results.each do |result|
 
-	report = Report.create(status: result.violstatus, level: result.viollevel, description: result.descript, comments: result.comments, date: result.issdttm, license: result.licenseno)
-	restaurant = Restaurant.find_or_create_by(name: result.businessname)
-	restaurant.address = result.address
-	restaurant.license = result.licenseno
-	restaurant.status = result.violstatus
-	restaurant.location = result.location
-	restaurant.reports << report
+# 	report = Report.create(status: result.violstatus, level: result.viollevel, description: result.descript, comments: result.comments, date: result.issdttm, license: result.licenseno)
+# 	restaurant = Restaurant.find_or_create_by(name: result.businessname)
+# 	restaurant.address = result.address
+# 	restaurant.license = result.licenseno
+# 	restaurant.status = result.violstatus
+# 	restaurant.location = result.location
+# 	restaurant.reports << report
 
-	restaurant.save
-end
+# 	restaurant.save
+# end
