335a6b6016d305b2ac9c86c1da553aa450d98ad4
Kathleen <kayjsmyth@gmail.com>
2016-12-11 19:38:26 -0500

    cleaned up controllers/concerns

--- config/application.rb
+++ config/application.rb
@@ -16,3 +16,38 @@ module Dontpuke
     config.action_controller.permit_all_parameters = true
   end
 end
+
+# require 'soda/client'
+
+# class InspectionsParser
+		
+# 	@@client = SODA::Client.new({:domain => "data.cityofboston.gov", :app_token => ""})
+
+# 	def self.parse
+# 		self.new.call
+# 	end
+
+# 	def call
+# 		get_everything
+# 	end
+
+# 	def get_everything
+# 		results = @@client.get("427a-3cn5", :$limit => 500000)
+# 		#go get stuff we want
+# 		results.each do |result|
+# 			restaurant = Restaurant.find_or_create_by(name: result.businessname)
+# 			restaurant.address = result.address
+# 			restaurant.license = result.licenseno
+# 			restaurant.status = result.violstatus
+# 			restaurant.location = result.location
+
+# 			report = Report.create(status: result.violstatus, level: result.viollevel, description: result.descript, comments: result.comments, date: result.statusdate, license: result.licenseno)
+# 			if report.license == restaurant.license
+# 				restaurant.reports << report
+# 			end
+# 			restaurant.save
+			
+# 		end
+# 		#byebug
+# 	end
+# end
