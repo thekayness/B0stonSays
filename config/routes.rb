1373f39658784ff2a07befe4745ced38f72be83e
Kathleen <kayjsmyth@gmail.com>
2016-12-11 19:36:37 -0500

    updated controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'restaurants#index'
-  resources :restaurants
-  get 'show' => 'restaurants#show'
-
-  get 'restaurants' => 'restaurants#find'
+  root 'home#index'
+ 
 end
